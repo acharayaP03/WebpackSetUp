e376bdf535b3aa90b426ce5868a7eebf113c9d09
prabhakar acharya <prabhakaracharya@prabhakar-acharyas-MacBook-Pro.local>
2019-10-27 11:50:55 +1100

    setting up webpack config file to run the npm script

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+//here we will tell webpack where to start. by setting up entry point
+var path = require('path');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename : 'bundle.js',
+        publicPath: '/dist'
+    }
+}
